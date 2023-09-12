111745a2d8d05dfbccca6c6793dce1dcc244b317
Bavo <61050421+Ovab@users.noreply.github.com>
2023-09-12 16:44:40 +0200

    Project pagina bijna af als je niet  denkt aan clickability

--- src/components/ProjectBlock.svelte
+++ src/components/ProjectBlock.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
     import Tags from "./tags.svelte";
+    export let name
     export let korteBeschrijving;
     export let tags;
+
+    export let img
 </script>
-
-<div class="w-[500px] h-[250px] rounded">
-    <img class="rounded" src="/assets/test.jpg" alt="yYEET">
-    <div class="relative -translate-y-[70px] bg-gray-800/70 h-[70px] rounded">
-        <p class="text-white ml-2">{korteBeschrijving}</p>
-
-        <Tags tagName="nuxt" tags="{tags}"/>
+<div class="group">
+<div class=" h-[250px] rounded bg-gray-500 group-hover:scale-105 transition-all">
+    <div class="flex justify-center overflow-hidden">
+            <img class="rounded object-cover h-[250px] group-hover:scale-110 transition-all" src="{img}" alt="yYEET">
     </div>
+    <div class="relative -translate-y-[70px] bg-gray-800/70 h-[70px] rounded flex flex-row">
+        <div class="w-[50%]">
+            <p class="text-white ml-2 font-semibold">{name}</p>
+            <Tags tagName="nuxt" tags="{tags}"/>
+        </div>
+
+        <p class="text-white ml-2">{korteBeschrijving}</p>
+    </div>
+</div>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,22 +3,41 @@
 
     export const tags = [
         {
-            "color": '#00dc82',
-            "name": "Nuxt"
+            "name": "Nuxt",
+            "color": '#00dc82'
         },
         {
-            "color":'#38bdf8',
-            "name": "Tailwind"
+            "name": "Tailwind",
+            "color":'#38bdf8'
+
+        },
+        {
+            "name": "Svelte",
+            "color":'#ff0080'
+            
+        },
+        {
+            "name": "Laravel",
+            "color": "#ff2e63"
         }
     ]
 </script>
 
-<div class="ml-10">
+<div class="mx-10">
     <h1 class="text-3xl">
         Projecten
     </h1>
 
-    <div class="grid col-span-3 grid-rows-3 mt-3">
-        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" url="/projects"/>
+    <h2 class="text-2xl text-center">Werk</h2>
+    <div class="grid grid-cols-3 grid-rows-1 mt-3 gap-2">
+        <ProjectBlock name="Jaspers Media" korteBeschrijving="sample text" tags="{tags}" img="/assets/logo-jm.png" url="/projects"/>
+        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" img="/assets/test.jpg" url="/projects"/>
+        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" img="/assets/test.jpg" url="/projects"/>
+    </div>
+    <h2 class="text-2xl text-center">Projecten</h2>
+    <div class="grid grid-cols-3 grid-rows-1 mt-3 gap-2">
+        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" img="/assets/test.jpg" url="/projects"/>
+        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" img="/assets/test.jpg" url="/projects"/>
+        <ProjectBlock korteBeschrijving="yeyeyey" tags="{tags}" img="/assets/test.jpg" url="/projects"/>
     </div>
 </div>
