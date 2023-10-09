0ae2ff399275c758af4e2446968302c39ead2b04
Bavo <61050421+Ovab@users.noreply.github.com>
2023-10-09 16:59:40 +0200

    API call werkey

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,6 +2,9 @@
     import {ofetch} from "ofetch";
     import {PUBLIC_API_URL} from "$env/static/public";
 
+    let projRes = null;
+    let werkRes = null;
+
     async function getTags() {
         const resTags = await ofetch(PUBLIC_API_URL + `getTags`, {
             method: 'POST',
@@ -12,14 +15,21 @@
     }
 
     async function getProjects() {
-        const resProj = await ofetch(PUBLIC_API_URL + `getProjects`, {
+        await ofetch(PUBLIC_API_URL + `getProjects`, {
             method: 'POST',
-        }).catch((err) => {
-            err.data;
-            alert('Er is iets misgegaan, stuur aub handmatig een email met de gegevens hier naast')
-        });
-        return resProj;
+        }).then((res) => {
+            projRes = res.projects;
+            werkRes = res.werk;
+            console.log(projRes[0], werkRes);
+            return res;
+        })
+            .catch((err) => {
+                err.data;
+                alert('Er is iets misgegaan, stuur aub handmatig een email met de gegevens hier naast')
+            });
     }
+
+    getProjects();
 
 </script>
 
@@ -29,9 +39,28 @@
 </div>
 
 <div class="flex flex-row w-screen min-h-[95vh]">
-    <div class="bg-green-300 w-[15%]">
-        haii :3
+    <div class="bg-green-300 w-[15%] flex flex-col gap-8 pl-2">
+        {#if projRes != null}
+            <div>
+                Werk
+                <div class="flex flex-col ml-2">
+                    {#each werkRes as werk}
+                        {werk.projName}
+                    {/each}
+                </div>
+            </div>
+
+            <div>
+                Projecten
+                <div class="flex flex-col ml-2">
+                    {#each projRes as project}
+                        {project.projName}
+                    {/each}
+                </div>
+            </div>
+        {/if}
     </div>
+
     <div class="bg-purple-500 w-full">
         yeyegtfyud
     </div>
