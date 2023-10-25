b35fdbf235afb382c7f8515a2ee91615329bf5d1
Bavo <61050421+Ovab@users.noreply.github.com>
2023-10-25 14:53:01 +0200

    Update app.cjs

--- app.cjs
+++ app.cjs
@@ -28,7 +28,8 @@ const conn = mysql.createConnection({
     host: "localhost",
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 // check if DB not ded
